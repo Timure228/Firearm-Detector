e4f12b2a4b473c12ca1c6ed58cfb68a92d1aaaca
Timure228 <arductimur@gmail.com>
2025-09-04 08:09:07 +0200

    commit

--- xception_model.py
+++ xception_model.py
@@ -1,42 +1,44 @@
 import keras
 from process_files import X_train, y_train, X_val, y_val
 
-imagenet = keras.applications.Xception(input_shape=(256, 256, 3),
-                                       classes=4,
-                                       include_top=False)
-# Edit layers
-input_ = keras.layers.Input((256, 256, 3))
-x = imagenet(input_, training=False)
-x = keras.layers.GlobalAvgPool2D()(x)
-x = keras.layers.Flatten()(x)
+# Import the model
+base_model = keras.applications.MobileNetV2(input_shape=(256, 256, 3),
+                                            include_top=False,
+                                            weights="imagenet")
+# Define custom layers
+input_layer = keras.layers.Input((256, 256, 3))
+x = base_model(input_layer, training=False)
+glob_avg_pool = keras.layers.GlobalAvgPool2D()(x)
+x = keras.layers.Flatten()(glob_avg_pool)
 x = keras.layers.Dense(256, activation="relu")(x)
-dropout_layer = keras.layers.Dropout(0.5)(x)
-output_ = keras.layers.Dense(4, activation="linear")(dropout_layer)
+batch_norm = keras.layers.BatchNormalization()(x)
+dropout_layer = keras.layers.Dropout(0.5)(batch_norm)
+output_layer = keras.layers.Dense(4, activation="linear")(dropout_layer)
 # Define the model
-xception_custom = keras.models.Model(input_, output_, name="xception_custom")
+custom_model = keras.Model(input_layer, output_layer, name="custom_model")
 # 1 Training Loop
-for layer in xception_custom.layers[50:]:
+for layer in base_model.layers:
     layer.trainable = False
 # Compile the model
 loss = keras.losses.CategoricalCrossentropy()
 optimizer = keras.optimizers.SGD(learning_rate=0.1, momentum=0.7)
-xception_custom.compile(loss=loss, optimizer=optimizer)
-history = xception_custom.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=5)
+custom_model.compile(loss=loss, optimizer=optimizer)
+history = custom_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=5)
 # 2 Training Loop
-for layer in xception_custom.layers[:60]:
+for layer in base_model.layers[35:]:
     layer.trainable = True
 # Compile the model
 loss = keras.losses.CategoricalCrossentropy()
 optimizer = keras.optimizers.SGD(learning_rate=0.01, momentum=0.7)
-xception_custom.compile(loss=loss, optimizer=optimizer)
-history = xception_custom.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=15)
+custom_model.compile(loss=loss, optimizer=optimizer)
+history = custom_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=15)
 # 3 Training Loop
-for layer in xception_custom.layers:
+for layer in base_model.layers:
     layer.trainable = True
 # Compile the model
 loss = keras.losses.CategoricalCrossentropy()
 optimizer = keras.optimizers.SGD(learning_rate=0.001, momentum=0.7)
-xception_custom.compile(loss=loss, optimizer=optimizer)
-history = xception_custom.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=25)
+custom_model.compile(loss=loss, optimizer=optimizer)
+history = custom_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=30)
 # Save the model
-xception_custom.save("models/custom_xception.keras")
+custom_model.save("models/custom1.keras")
